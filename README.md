5ff3019d249e3239de459bd384d3fa564da515dd
fightingggggg <GitHub choijihye601@gmail.com>
2025-05-08 14:16:13 +0900

    server

--- README.md
+++
@@ -1,2 +0,0 @@
-# dashbord_real
-# dashbboard
--- server/index.ts
+++ server/index.ts
@@ -59,7 +59,7 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const PORT = process.env.PORT || 5001;
+  const PORT = process.env.PORT || 8080;
   server.listen(PORT, () => {
     console.log('Server running on http://localhost:5000');
   });
