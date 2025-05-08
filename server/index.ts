a5ac1d3cc5b538c95424f736a14f1aba8aae9b5a
fightingggggg <GitHub choijihye601@gmail.com>
2025-05-08 14:28:51 +0900

    배포:

--- server/index.ts
+++ server/index.ts
@@ -60,7 +60,7 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const PORT = process.env.PORT || 8080;
-  server.listen(PORT, () => {
+  app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 
   });
