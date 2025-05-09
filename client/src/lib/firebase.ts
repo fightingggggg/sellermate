5a80ca76c76e9bba3b5c0664608ba6e2873d21f2
fightingggggg <GitHub choijihye601@gmail.com>
2025-05-10 01:11:26 +0900

    api 키

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -3,11 +3,11 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAxZ0lTXab1JALiNnHKAx2_N7hlr84YSN0",
+  apiKey: process.env.apiKey,
   authDomain: "smartstoreseo.firebaseapp.com",
   projectId: "smartstoreseo",
   storageBucket:  "smartstoreseo.firebasestorage.app",
-  appId:"1:1034657335294:web:c3d36cc05995a9f078e2af",
+  appId: process.env.appId
 };
 
 // Initialize Firebase
