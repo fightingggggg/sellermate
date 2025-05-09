9219d472c4adfa500c26bfa49c4138e0ef0c91d5
fightingggggg <GitHub choijihye601@gmail.com>
2025-05-10 01:24:12 +0900

    그냥

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -3,11 +3,11 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
+  apiKey: "AIzaSyAxZ0lTXab1JALiNnHKAx2_N7hlr84YSN0",
   authDomain: "smartstoreseo.firebaseapp.com",
   projectId: "smartstoreseo",
   storageBucket:  "smartstoreseo.firebasestorage.app",
-  appId: process.env.appId
+  appId:"1:1034657335294:web:c3d36cc05995a9f078e2af",
 };
 
 // Initialize Firebase
