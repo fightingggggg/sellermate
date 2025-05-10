200de852c9ce888cd94e9fa116a0e2ba03727f48
fightingggggg <GitHub choijihye601@gmail.com>
2025-05-11 03:19:10 +0900

    서버오류

--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,13 @@ import { setupVite, serveStatic, log } from "./vite";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static('client/dist', {
+  setHeaders: (res, path) => {
+    if (path.endsWith('.js')) {
+      res.setHeader('Content-Type', 'application/javascript');
+    }
+  }
+}));
 const PORT = process.env.PORT || 8081;
 
 app.use((req, res, next) => {
